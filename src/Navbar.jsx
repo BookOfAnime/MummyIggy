c8f6ab0d56acb60ff82f7d88216f8c781148621e
Ephriam Kassa <ephriam123@gmail.com>
2024-06-06 21:28:13 -0700

    sssadasd

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navbar-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="https://pump.fun/board">PUMP</a></li>
+        <li><a href="https://mummyiggy.netlify.app/">Home</a></li>
+        <li><a href="https://pump.fun/9WKf7pz7eoioVoyYSsLEjH5D6aGsPMUm51wuqgr4pump">PUMP</a></li>
         <li><a href="https://t.me/MummyIggyPortal">Telegram</a></li>
       </ul>
     </nav>
